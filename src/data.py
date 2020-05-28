8707cfe797f3e1377abbcd057278ded2b70fdf06
edgaregurrola <edgaregurrola@hotmail.com>
2020-05-28 01:14:31 -0500

    Function for downloading covid data from municipalities

--- src/data.py
+++ src/data.py
@@ -1,24 +1,41 @@
-#Codigo para descargar los datos de contagios de COVID-19 por municipio de la Secretaria de Salud
-#Observatorio de Ciudades
+################################################################################
+# Module: Data downloader
+# Saves the data from Secretaria de Salud in the folder ../data/raw/{date}.{format}
+# developed by: Edgar Egurrola
+# 			  edgar.egurrola@tec.mx
+# updated: 28/05/2020
+################################################################################
+
+import os
+import sys
 
 import requests
 import zipfile
+from datetime import date
+from datetime import timedelta
+#import src
 
-#Ubicacion para guardar el archivo
-ubicacion = 'D:\\Users\\edgar\\Source\\Repos\\Observatorio-Ciudades\\riesgo-municipal\\data\\raw'
+def covid_mun ():
+    """Download csv file with daily updates from the Secretaria de Salud about COVID-19 by municipality
+    
+    """
 
-#url para descargar el archivo
-url = 'http://187.191.75.115/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip'
+    #Saved data location
+    ubicacion = 'D:\\Users\\edgar\\Source\\Repos\\Observatorio-Ciudades\\riesgo-municipal\\data\\raw\\municipalities\\'+str(date.today()-timedelta(days=1))
+    os.mkdir(ubicacion) 
 
-r = requests.get(url)
+    #data download url
+    url = 'http://187.191.75.115/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip'
 
-#Nombre del archivo y ubicacion en el que se guarda el zip
-archivo = ubicacion + '\\' + 'datos_abiertos_covid19.zip'
+    r = requests.get(url)
 
-#Escribe el archivo zip
-with open (archivo, 'wb') as code:
-	code.write(r.content)
+    #Name and location to save data
+    archivo = ubicacion + '\\' + 'datos_abiertos_covid19.zip'
 
-#Escribe el archivo zip
-with zipfile.ZipFile(archivo, 'r') as zip_ref:
-	zip_ref.extractall(ubicacion)
+    #Escribe el archivo zip
+    with open (archivo, 'wb') as code:
+        code.write(r.content)
+
+    #Escribe el archivo zip
+    with zipfile.ZipFile(archivo, 'r') as zip_ref:
+        zip_ref.extractall(ubicacion)
